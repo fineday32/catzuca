f0827c72a76ae2b7922430b684c3c85e37d64cde
ChunYen <hcythomas0125@gmail.com>
2013-10-22 20:30:56 +0800

    Start

---
+++ catzuca/catzucaViewController.m
@@ -0,0 +1,29 @@
+//
+//  catzucaViewController.m
+//  catzuca
+//
+//  Created by Thomas on 13/10/22.
+//  Copyright (c) 2013年 Thomas Hsu. All rights reserved.
+//
+
+#import "catzucaViewController.h"
+
+@interface catzucaViewController ()
+
+@end
+
+@implementation catzucaViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ catzuca/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  catzuca
+//
+//  Created by Thomas on 13/10/22.
+//  Copyright (c) 2013年 Thomas Hsu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "catzucaAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([catzucaAppDelegate class]));
+    }
+}
