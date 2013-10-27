5158b1f21dd557fdd9af58f283dd92395e6ba62b
ChunYen <hcythomas0125@gmail.com>
2013-10-27 17:11:43 +0800

    lastes version

--- catzuca/mainVC.m
+++ catzuca/mainVC.m
@@ -8,12 +8,15 @@
 
 #import "mainVC.h"
 #import "Constant.h"
+#import "KxMenu.h"
 
 @interface mainVC ()
 
 @end
 
-@implementation mainVC
+@implementation mainVC{
+    UIButton *menuButton;
+}
 
 - (id)initWithStyle:(UITableViewStyle)style
 {
@@ -33,6 +36,18 @@
  
     // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
     // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    
+    menuButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    menuButton.frame = CGRectMake(230, 10, 100, 50);
+    [menuButton setTitle:@"MENU" forState:UIControlStateNormal];
+    [menuButton addTarget:self action:@selector(showMenu:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:menuButton];
+
+    [_upperBarImage setBackgroundColor:[UIColor cyanColor ]];
+//    [_upperBarImage setBackgroundColor:[UIColor colorWithRed:148/255 green:183/255 blue:210/255 alpha:0]];
+
+
+
 }
 
 - (void)didReceiveMemoryWarning
@@ -47,14 +62,14 @@
 {
 #warning Potentially incomplete method implementation.
     // Return the number of sections.
-    return 2;
+    return 0;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
 #warning Incomplete method implementation.
     // Return the number of rows in the section.
-    return 2;
+    return 0;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
@@ -64,7 +79,6 @@
     
     // Configure the cell...
     
-    cell.textLabel.text = @"";
     return cell;
 }
 
@@ -123,5 +137,43 @@
 }
 
  */
+
+- (void)showMenu:(UIButton *)sender
+{
+    NSArray *menuItems =
+    @[
+      
+      [KxMenuItem menuItem:@"小 小 步 落 客"
+                     image:nil
+                    target:nil
+                    action:NULL],
+      
+      [KxMenuItem menuItem:@"我 的 照 片"
+                     image:[UIImage imageNamed:@"action_icon"]
+                    target:self
+                    action:@selector(pushMenuItem:)],
+      
+      [KxMenuItem menuItem:@"我 的 影 片"
+                     image:[UIImage imageNamed:@"check_icon"]
+                    target:self
+                    action:@selector(pushMenuItem:)],
+      
+
+      ];
+    
+    KxMenuItem *first = menuItems[0];
+    first.foreColor = [UIColor colorWithRed:47/255.0f green:112/255.0f blue:225/255.0f alpha:1.0];
+    first.alignment = NSTextAlignmentCenter;
+    
+    [KxMenu showMenuInView:self.view
+                  fromRect:sender.frame
+                 menuItems:menuItems];
+}
+
+- (void) pushMenuItem:(id)sender
+{
+    NSLog(@"%@", sender);
+}
+
 
 @end
