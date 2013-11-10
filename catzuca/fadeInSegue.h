a5c687e3f0d0b01f8e0f28f27aee5fcc4bf35e49
cytms <cytmos@gmail.com>
2013-11-10 18:02:49 +0800

    add search

---
+++ catzuca/fadeInSegue.h
@@ -0,0 +1,13 @@
+//
+//  fadeInSegue.h
+//  catzuca
+//
+//  Created by cytms on 13/11/10.
+//  Copyright (c) 2013年 Thomas Hsu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface fadeInSegue : UIStoryboardSegue
+
+@end
---
+++ catzuca/fadinInSegue.h
@@ -0,0 +1,13 @@
+//
+//  fadinInSegue.h
+//  catzuca
+//
+//  Created by cytms on 13/11/10.
+//  Copyright (c) 2013年 Thomas Hsu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface fadinInSegue : UIStoryboardSegue
+
+@end
